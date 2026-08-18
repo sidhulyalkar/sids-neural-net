c1fa85723db06d9acf612b2282e04c970bf36c41
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-08-18 13:35:29 -0700

    feat(physiology): include PhysioPersona in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -19,6 +19,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     '/archive',
     '/photography',
     '/ideas',
+    '/physiology',
     '/timeline',
     '/case-studies',
     '/about',
