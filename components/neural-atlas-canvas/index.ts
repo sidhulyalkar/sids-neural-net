8e42225afdb2b8e313422bcd2c97eafc04a4e569
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-08-23 16:16:26 -0700

    Route homepage through adaptive fractal presentation stage

--- components/neural-atlas-canvas/index.ts
+++ components/neural-atlas-canvas/index.ts
@@ -1,2 +1,3 @@
-export { AdaptiveFractalHome } from './AdaptiveFractalHome';
+export { AdaptiveFractalStage as AdaptiveFractalHome } from './AdaptiveFractalStage';
+export { AdaptiveFractalHome as AdaptiveFractalHomeBase } from './AdaptiveFractalHome';
 export { MinimalDendriteHome } from './MinimalDendriteHome';
