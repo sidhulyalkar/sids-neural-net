d1d2a072334e0bfb319449a31a832af8acd4e46a
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-08-23 19:58:09 -0700

    Wire Echo Nest v3 and quiet Core placement

--- components/neural-atlas-canvas/AdaptiveFractalStage.tsx
+++ components/neural-atlas-canvas/AdaptiveFractalStage.tsx
@@ -3,12 +3,14 @@
 import { FractalThemeRecorder } from '@/components/neural-atlas/FractalThemeEcho';
 import { AdaptiveFractalHome as AdaptiveFractalHomeBase } from './AdaptiveFractalHome';
 import { FractalSurfaceEnhancerV2 } from './FractalSurfaceEnhancerV2';
+import { FractalExperienceV3 } from './FractalExperienceV3';
 
 export function AdaptiveFractalStage() {
   return (
     <>
       <AdaptiveFractalHomeBase />
       <FractalSurfaceEnhancerV2 />
+      <FractalExperienceV3 />
       <FractalThemeRecorder />
     </>
   );
