2bddc3640e40848e43c882c7b7aeba55adc29303
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-08-24 15:12:59 -0700

    Wire crisp topology renderer into fractal stage

--- components/neural-atlas-canvas/AdaptiveFractalStage.tsx
+++ components/neural-atlas-canvas/AdaptiveFractalStage.tsx
@@ -2,6 +2,7 @@
 
 import { FractalThemeRecorder } from '@/components/neural-atlas/FractalThemeEcho';
 import { AdaptiveFractalHome as AdaptiveFractalHomeBase } from './AdaptiveFractalHome';
+import { FractalCrispTopologyV13 } from './FractalCrispTopologyV13';
 import { FractalSurfaceEnhancerV2 } from './FractalSurfaceEnhancerV2';
 import { FractalExperienceV3 } from './FractalExperienceV3';
 
@@ -9,6 +10,7 @@ export function AdaptiveFractalStage() {
   return (
     <>
       <AdaptiveFractalHomeBase />
+      <FractalCrispTopologyV13 />
       <FractalSurfaceEnhancerV2 />
       <FractalExperienceV3 />
       <FractalThemeRecorder />
