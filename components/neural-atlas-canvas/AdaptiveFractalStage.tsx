c4fc3ad601a7b3fe5daeea0212b785f07c3baa9a
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-08-26 15:54:09 -0700

    Mount adaptive interior density canopy

--- components/neural-atlas-canvas/AdaptiveFractalStage.tsx
+++ components/neural-atlas-canvas/AdaptiveFractalStage.tsx
@@ -4,6 +4,7 @@ import { FractalThemeRecorder } from '@/components/neural-atlas/FractalThemeEcho
 import { AdaptiveFractalHome as AdaptiveFractalHomeBase } from './AdaptiveFractalHome';
 import { FractalCrispTopologyV13 } from './FractalCrispTopologyV13';
 import { FractalExperienceV3 } from './FractalExperienceV3';
+import { FractalInteriorDensityV17 } from './FractalInteriorDensityV17';
 import { FractalPublicCurationV14 } from './FractalPublicCurationV14';
 import { FractalResponsiveEnvelopeV16 } from './FractalResponsiveEnvelopeV16';
 import { FractalSurfaceEnhancerV2 } from './FractalSurfaceEnhancerV2';
@@ -17,6 +18,7 @@ export function AdaptiveFractalStage() {
       <FractalExperienceV3 />
       <FractalPublicCurationV14 />
       <FractalResponsiveEnvelopeV16 />
+      <FractalInteriorDensityV17 />
       <FractalThemeRecorder />
     </>
   );
