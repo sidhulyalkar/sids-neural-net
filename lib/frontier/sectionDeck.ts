f1dfbb4332cf5808dd74b5a7b5e10e0f9205c66b
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-09-08 23:23:05 -0700

    perf(frontier): raise bounded page density

--- lib/frontier/sectionDeck.ts
+++ lib/frontier/sectionDeck.ts
@@ -1,8 +1,8 @@
 import { FRONTIER_LANE_MAP } from './config';
 import type { FrontierItem, FrontierLaneId } from './types';
 
-export const FRONTIER_SECTION_PAGE_SIZE = 6;
-export const FRONTIER_SECTION_FEED_PAGE_SIZE = 2;
+export const FRONTIER_SECTION_PAGE_SIZE = 8;
+export const FRONTIER_SECTION_FEED_PAGE_SIZE = 3;
 
 export type FrontierSectionPage = {
   id: string;
